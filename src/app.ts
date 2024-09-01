780da4a12bff60255abf02a542d66fce63bee3ab
Daniel Nguyen <83233160+godliketoc6@users.noreply.github.com>
2024-09-01 19:09:01 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:5173", // or your frontend URL
+    origin: ['http://localhost:5173', 'https://gold-crowfunding-blockchain.netlify.app/'], // or your frontend URL
     credentials: true, // This is important for cookies/sessions
 }));
 
